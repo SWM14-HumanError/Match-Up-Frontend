60b3d23874231bc07ee5bfbd549c777dd0fdb22a
Hyunwoo <hyunwoo0081@gmail.com>
2023-07-12 17:38:33 +0900

    Chore: vite, pnpm 프로젝트 설정

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true
+      }
+    }
+  }
+})
