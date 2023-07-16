2eae26e43207756fdb240247f9500668ac4ab61f
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-07-16 23:42:49 +0900

    Fix: 메인페이지 디자인 수정

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,38 +1,44 @@
 interface IProjectCard {
-    teamId: number,
-    teamName: string,
-    teamImage: string,
-    teamDescription: string,
-    teamStar: number,
+  teamId: number,
+  teamName: string,
+  teamImage: string,
+  teamDescription: string,
+  teamStar: number,
 }
 
 function ProjectCard({teamId, teamName, teamImage, teamDescription, teamStar}: IProjectCard) {
 
-    return (
+  return (
+    <div className='project_card'>
+      <div>
         <div>
-            <div>
-                <img src={teamImage} alt="team image"/>
-                <div>
-                    <div>
-                        <h3>{teamName}</h3>
-                        <button>
-                            <img src="" alt="heart"/>
-                        </button>
-                    </div>
-                    <p>{teamDescription}</p>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <img src="" alt="star"/>
-                    <p>{teamStar}</p>
-                </div>
-                <div>
-                    <button>{teamId} 모임 바로가기</button>
-                </div>
-            </div>
+          <div className='name_layout'>
+            <h3>{teamName}</h3>
+            <button>
+              <img
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png'
+                alt='heart'/>
+            </button>
+          </div>
+          <div className='info_layout'>
+            <img src={teamImage} alt='team image'/>
+            <p>{teamDescription}</p>
+          </div>
         </div>
-    );
+      </div>
+      <div className='project_links'>
+        <div className='project_likes'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png'
+            alt='star'/>
+          <p>{teamStar}</p>
+        </div>
+        <div>
+          <button>{teamId} 모임 바로가기</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -2,8 +2,8 @@ function SelectBox() {
   return (
     <div>
       <select>
-        <option value="프로젝트">프로젝트</option>
-        <option value="스터디">스터디</option>
+        <option value='프로젝트'>프로젝트</option>
+        <option value='스터디'>스터디</option>
       </select>
     </div>
   )
