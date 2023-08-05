0df77a30fec75a61e06b0b4d380690bb1d9e8df5
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-08-05 18:01:43 +0900

    Fix: 기술 스택 이미지 방식 변경으로 인한, 이미지 경로 변경

--- src/components/StackImage.tsx
+++ src/components/StackImage.tsx
@@ -1,7 +1,20 @@
+import {useState} from 'react';
+
 function StackImage({stack}: IStackImage) {
+  const normalizedStack = stack.toLowerCase().replace(/\./g, '');
+  const [url, setUrl] = useState<string>(
+    `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${normalizedStack}/${normalizedStack}-original.svg`);
+
+  function loadOtherImage() {
+    setUrl(`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${normalizedStack}/${normalizedStack}-plain.svg`);
+  }
+
   return (
     <div className='stack_layout'>
-      <img src={`/assets/stacks/${stack}.png`} alt={stack}/>
+      {/*<img src={`/assets/stacks/${stack}.png`} alt={stack}/>*/}
+      <img src={url}
+           onError={loadOtherImage}
+           alt={stack}/>
       <span>{stack}</span>
     </div>
 );
