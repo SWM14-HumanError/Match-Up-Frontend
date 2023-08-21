524b5844db032be6f5c7308769e179ff362dd2d0
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-08-21 22:59:45 +0900

    Fix: 기술 스택 관련 CSS 오류 수정

--- src/components/StackImage.tsx
+++ src/components/StackImage.tsx
@@ -7,6 +7,7 @@ interface IStackImage {
 
 function StackImage({stack}: IStackImage) {
   const normalizedStack = stack.tagName.toLowerCase().replace(/\./g, '');
+  const [isHover, setIsHover] = useState<boolean>(false);
   const [url, setUrl] = useState<string>(
     stack.url != null ?
       `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${stack.url}.svg` :
@@ -17,11 +18,14 @@ function StackImage({stack}: IStackImage) {
   }
 
   return (
-    <div className='stack_layout'>
+    <div className='stack_layout'
+         onClick={e => e.stopPropagation()}>
       <img src={url}
            onError={loadOtherImage}
-           alt={stack.tagName}/>
-      <span>{stack.tagName}</span>
+           alt={stack.tagName}
+           onMouseEnter={() => setIsHover(true)}
+           onMouseLeave={() => setIsHover(false)}/>
+      <span className={isHover ? 'visible' : ''}>{stack.tagName}</span>
     </div>
   );
 }
