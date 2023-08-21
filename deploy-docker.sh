c0410fa372cc4b0400ca83b55e5fc20fde5a4aab
jujemu <jujemu30@gmail.com>
2023-08-22 02:57:31 +0900

    Feat: Edit nginx configure, build.sh

--- deploy-docker.sh
+++
@@ -1,6 +0,0 @@
-# npm install --dev @types/react
-npm run build
-docker rmi jujemu/oauth-test:client
-# docker build --platform linux/amd64 --tag jujemu/oauth-test:client -f ./Dockerfile  .
-docker build --tag jujemu/oauth-test:client -f ./Dockerfile  .
-docker push jujemu/oauth-test:client
