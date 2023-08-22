21b0b86eeeead3d00ed42c54a1ad6df897d18cea
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-08-22 15:16:24 +0900

    Fix: Vite Proxy 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+const TargetServer = 'http://localhost:8080';
+// const TargetServer = 'http://15.165.24.191:8080';
+
 export default defineConfig({
   plugins: [react()],
   base: './',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        // target: 'http://15.165.24.191:8080',
+        target: TargetServer,
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
         ws: true
       },
-      '^/login$': {
-        target: 'http://localhost:80',
-        // target: 'http://15.165.24.191:8080',
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      },
-      '^/logout$': {
-        target: 'http://localhost:80',
-        // target: 'http://15.165.24.191:8080',
+      '^/(login|logout)$': {
+        target: TargetServer,
         changeOrigin: true,
         secure: false,
         ws: true
