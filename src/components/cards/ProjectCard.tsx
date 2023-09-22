056c5f873e85eb754857586643f8484c45c1e874
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-09-23 00:02:42 +0900

    Feat: Project 카드 좋아요 기능 추가

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,16 +1,34 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Like from '../svgs/Like.tsx';
 import HeartCount from '../svgs/HeartCount.tsx';
 import TierSvg from '../svgs/Tier/TierSvg.tsx';
 import StackImage from '../StackImage.tsx';
 import {ITeamProjectSummary} from '../../constant/interfaces.ts';
+import Api from '../../constant/Api.ts';
 import '../../styles/components/ProjectCard.scss';
 
 function ProjectCard({id, title, description, like, thumbnailUrl, techStacks, leaderID, leaderName, leaderLevel}: ITeamProjectSummary) {
   const navigate = useNavigate();
-  const [heart, setHeart] = useState(false);
+  const [heart, setHeart] = useState<boolean>(false);
+  const [prevHeart, setPrevHeart] = useState<boolean>(false);
 
+  // Todo: 이전에 Heart를 눌렀는지 확인하는 API 추가 요청 / useLikeQuery로 분리 하기
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      if (prevHeart === heart) return;
+
+      setPrevHeart(heart);
+      if (heart) {
+        Api.fetch2Json(`/api/v1/team/${id}/like`, 'POST').then();
+      } else {
+        Api.fetch2Json(`/api/v1/team/${id}/like`, 'DELETE').then();
+      }
+    }, 700);
+
+    return () => clearTimeout(debounceTimer);
+  }, [heart]);
+  
   return (
     <div className='project_card' onClick={() => navigate(`/project/${id}`)}>
       {thumbnailUrl ? (
@@ -29,7 +47,6 @@ function ProjectCard({id, title, description, like, thumbnailUrl, techStacks, le
             <button className='image_button' onClick={event => {
               event.stopPropagation();
               setHeart(prev => !prev);
-              // Todo: like
             }}>
               <Like enable={heart}/>
             </button>
@@ -37,12 +54,16 @@ function ProjectCard({id, title, description, like, thumbnailUrl, techStacks, le
 
           <p>{description}</p>
 
-          <h4>프로젝트 스택</h4>
-          <ul>
-            {techStacks.map((stack, index) => (
-              <li key={index}><StackImage stack={stack}/></li>
-            ))}
-          </ul>
+          { techStacks.length > 0 && (
+            <>
+              <h4>프로젝트 스택</h4>
+              <ul>
+                {techStacks.map((stack, index) => (
+                  <li key={index}><StackImage stack={stack}/></li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
 
         <div className='project_user_layout'>
