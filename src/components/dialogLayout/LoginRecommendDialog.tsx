6a57807cc74b3d5a29c56b049b41f333ede1bfc3
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-10-02 14:17:26 +0900

    Feat: 로그인 권유 모달창 디자인 및 수정

--- src/components/dialogLayout/LoginRecommendDialog.tsx
+++ src/components/dialogLayout/LoginRecommendDialog.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import LOGO from '../../../assets/LOGO.png';
 import CloseIcon from '../svgs/CloseIcon.tsx';
 import DialogTemplate from './DialogTemplate.tsx';
 import authControl from '../../constant/authControl.ts';
+
+import '../../styles/dialogs/LoginRecommendDialog.scss';
 
 interface ILoginRecommendDialog {
   isOpen: boolean;
@@ -12,7 +15,7 @@ interface ILoginRecommendDialog {
 function LoginRecommendDialog({isOpen, setIsOpen}:ILoginRecommendDialog) {
   return (
     <DialogTemplate isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className='apply_dialog'>
+        <div className='login_dialog_layout'>
           <div className='dialog_header'>
             <div>
               <h3>로그인</h3>
@@ -25,13 +28,16 @@ function LoginRecommendDialog({isOpen, setIsOpen}:ILoginRecommendDialog) {
             </div>
           </div>
 
-          <div className='dialog_content'>
+          <div className='dialog_content text_center'>
+            <img className='logo' src={LOGO} alt='Match up logo'/>
+
             <p>로그인을 하면 더 많은 정보를 볼 수 있습니다</p>
+            <p>로그인히여 팀원을 구하고, 마음에 드는 프로젝트에 지원해보세요</p>
           </div>
 
           <div className='dialog_footer fill'>
             <button onClick={() => authControl.login()}>
-              로그인/회원가입
+              로그인/가입
             </button>
           </div>
         </div>
