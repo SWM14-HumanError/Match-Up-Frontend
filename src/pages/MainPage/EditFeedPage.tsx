e4894d46e786b82a56e6a434a2ef3fec55633f8e
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-10-04 19:11:37 +0900

    Fix: 피드 생성 페이지 select option 변경

--- src/pages/MainPage/EditFeedPage.tsx
+++ src/pages/MainPage/EditFeedPage.tsx
@@ -5,7 +5,7 @@ import SelectBox from '../../components/inputs/SelectBox.tsx';
 import ImgUpload from '../../components/inputs/ImgUpload.tsx';
 import {IEditFeedInfo} from '../../constant/interfaces.ts';
 import {InitFeedInfo} from '../../constant/initData.ts';
-import {ProjectFields} from '../../constant/selectOptions.ts';
+import {ProjectSubFields} from '../../constant/selectOptions.ts';
 import Alert from '../../constant/Alert.ts';
 import Api from '../../constant/Api.ts';
 
@@ -74,7 +74,7 @@ function EditFeedPage() {
                          onChange={value =>
                            setFeedInfo(prev => ({...prev, type: ProjectTypeArr.indexOf(value)}))}/>
 
-              <SelectBox options={ProjectFields}
+              <SelectBox options={ProjectSubFields}
                          value={feedInfo.domain}
                          onChange={value =>
                            setFeedInfo(prev => ({...prev, domain: value}))}/>
