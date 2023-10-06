9f87c79a115d25cff13d792d6a4654dfc7396a5e
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-10-06 16:20:36 +0900

    Feat: Navigation 알림 메뉴 창 생성

--- src/components/navigation/AlarmModal.tsx
+++ src/components/navigation/AlarmModal.tsx
@@ -55,6 +55,8 @@ function AlarmModal({setIsAlarmModalOpened, target, setHasAlarm}: IAlarmModal) {
   const navigate = useNavigate();
   const infScrollRef = useRef(null);
   const [selectedCategory, setSelectedCategory] = useState(0);
+  const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
+  const [AlarmMenuData, setAlarmMenuData] = useState<IAlarmMenu>(InitAlarmMenu);
   
   const { data, setReqParams }
     = useInfScroll4Widget<IAlarmList>('/api/v1/alert', 'alertResponseList', infScrollRef, InitialAlarmData, {page: 0});
@@ -84,82 +86,93 @@ useEffect(() => {
 
   // @ts-ignore
   return (
-    <div className='modal_background alarm_modal'
-         style={{left: adjustedX}}
-         onClick={e => e.stopPropagation()}>
-      <div className='alarm_header_layout'>
-        <div className='header_title_layout'>
-          <Bell width={28} height={28} state={0}/>
-          <h3>알림</h3>
+    <>
+      {isMenuOpened && <AlarmMenu {...AlarmMenuData}/> }
+      <div className='modal_background alarm_modal'
+           style={{left: adjustedX}}
+           onClick={e => e.stopPropagation()}>
+        <div className='alarm_header_layout'>
+          <div className='header_title_layout'>
+            <Bell width={28} height={28} state={0}/>
+            <h3>알림</h3>
+          </div>
+
+          <div className='header_button_layout'>
+            <button className='link' onClick={authControl.logout}>로그아웃</button>
+            <button className='svg_button' onClick={() => navigate('/profile/settings')}>
+              <Settings width={28} height={28}/>
+            </button>
+            <button className='svg_button'
+                    onClick={() => setIsAlarmModalOpened(false)}>
+              <CloseIcon width={28} height={28}/>
+            </button>
+          </div>
         </div>
 
-        <div className='header_button_layout'>
-          <button className='link' onClick={authControl.logout}>로그아웃</button>
-          <button className='svg_button' onClick={() => navigate('/profile/settings')}>
-            <Settings width={28} height={28}/>
-          </button>
-          <button className='svg_button'
-                  onClick={() => setIsAlarmModalOpened(false)}>
-            <CloseIcon width={28} height={28}/>
-          </button>
-        </div>
-      </div>
-
-      <div className='alarm_content_layout'>
-        <div className='category_layout_container'>
-          <ul className='category_layout'>
-            {AlarmCategories.map((category, index) => (
-              <li key={category.name}>
-                <button className={selectedCategory === index ? 'selected' : ''}
-                        onClick={() => setSelectedCategory(index)}>{category.name}</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='alarm_contents_container'>
-          <ul className='alarm_contents'>
-            {dataIsEmpty(data) ? (
-                <li><div className='alarm_content read'><p>알림이 없습니다</p></div></li>
-              ) :
-              data.alertResponseList.map((data: JSX.IntrinsicAttributes & IAlarmContent) => data && (
-                <AlarmContent key={data.id} {...data} setIsAlarmModalOpened={setIsAlarmModalOpened}/>
+        <div className='alarm_content_layout'>
+          <div className='category_layout_container'>
+            <ul className='category_layout'>
+              {AlarmCategories.map((category, index) => (
+                <li key={category.name}>
+                  <button className={selectedCategory === index ? 'selected' : ''}
+                          onClick={() => setSelectedCategory(index)}>{category.name}</button>
+                </li>
               ))}
-          </ul>
+            </ul>
+          </div>
+          <div className='alarm_contents_container'>
+            <ul className='alarm_contents'>
+              {dataIsEmpty(data) ? (
+                  <li><div className='alarm_content read'><p>알림이 없습니다</p></div></li>
+                ) :
+                data.alertResponseList.map((data: JSX.IntrinsicAttributes & IAlarmContent) => data && (
+                  <AlarmContent key={data.id}
+                                {...data}
+                                setIsAlarmModalOpened={setIsAlarmModalOpened}
+                                setIsMenuOpened={setIsMenuOpened}
+                                setAlarmMenuData={setAlarmMenuData} />
+                ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 interface IAlarmContent extends IAlarmData{
   setIsAlarmModalOpened: (isAlarmModalOpened: boolean) => void;
+  setIsMenuOpened: (isMenuOpened: boolean) => void;
+  setAlarmMenuData: (alarmMenuData: IAlarmMenu) => void;
 }
 
-function AlarmContent({id, title, createdDate, content, redirectUrl, read, setIsAlarmModalOpened} : IAlarmContent) {
+function AlarmContent({id, title, createdDate, content, redirectUrl, read, setIsAlarmModalOpened, setIsMenuOpened, setAlarmMenuData} : IAlarmContent) {
   const navigate = useNavigate();
   // const [buttenHover, setButtonHover] = useState(false);
 
   function readAlarm() {
-    Api.fetch2Json(`/api/v1/alert/read/${id}`, 'POST')
-      .then((res) => {
-        setIsAlarmModalOpened(false);
-        console.log(res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    if (!read)
+      Api.fetch2Json(`/api/v1/alert/read/${id}`, 'POST')
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
+  }
 
-    navigate(redirectUrl)
+  function readAlarmAndClose() {
+    readAlarm();
+    navigate(redirectUrl);
+    setIsAlarmModalOpened(false);
   }
 
   function openAlarmMenu(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     e.stopPropagation();
+    setAlarmMenuData(InitAlarmMenu);
+    setIsMenuOpened(true);
   }
 
   return (
     <li>
       <div className={'alarm_content' + (read ? ' read' : '')}
-           onClick={readAlarm}>
+           onClick={readAlarmAndClose}>
         <div className='alarm_content_header'>
           <div>
             <h4>{title}</h4>
@@ -168,11 +181,34 @@ function AlarmContent({id, title, createdDate, content, redirectUrl, read, setIs
           <button className='image_button' onClick={openAlarmMenu}>
             <CircleHamburger width={16} height={16}/>
           </button>
+
         </div>
         <p>{content}</p>
       </div>
     </li>
   );
+}
+
+interface IAlarmMenu {
+  read: boolean;
+  readAlarm: () => void;
+}
+
+const InitAlarmMenu: IAlarmMenu = {
+  read: false,
+  readAlarm: () => {},
+}
+
+function AlarmMenu({read, readAlarm}: IAlarmMenu) {
+  return true ? null : (
+    <div className='modal_menu_background'>
+      <div className='alarm_content_menu'>
+        {!read && (
+          <button onClick={readAlarm}>읽음으로 표시</button>)}
+        <button>알림 삭제</button>
+      </div>
+    </div>
+  )
 }
 
 export default AlarmModal;
