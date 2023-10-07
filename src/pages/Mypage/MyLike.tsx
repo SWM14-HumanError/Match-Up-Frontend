553ff7c722201338051d2bd3708ad134b27c6ce4
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-10-07 18:55:06 +0900

    Feat: 좋아요 페이지 API 연결

--- src/pages/Mypage/MyLike.tsx
+++ src/pages/Mypage/MyLike.tsx
@@ -1,27 +1,19 @@
 import {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
 import Navigation from '../../components/navigation/Navigation.tsx';
 import ProjectCard from '../../components/cards/ProjectCard.tsx';
+import UserCard from '../../components/cards/UserCard.tsx';
 import Footer from '../../components/Footer.tsx';
 import LoginRecommendDialog from '../../components/dialogLayout/LoginRecommendDialog.tsx';
-import {IMyPageDetail} from '../../constant/interfaces.ts';
-import {InitMyPageDetail} from '../../constant/initData.ts';
-import {MyUserDetailDummy} from '../../dummies/dummyData.ts';
-import authControl from '../../constant/authControl.ts';
+import InfScroll from '../../constant/InfScroll.ts';
 import Api from '../../constant/Api.ts';
 
 import '../../styles/MainProjectPage.scss';
 
 function MyLike() {
-  const [myPageDetail, setMyPageDetail] = useState<IMyPageDetail>(InitMyPageDetail);
   const [isLoginDialogOpen, setIsLoginDialogOpen] = useState<boolean>(false);
-  const myID = authControl.getUserIdFromToken();
-
-  useEffect(() => {
-    Api.fetch2Json(`/api/v1/profile/${myID}`)
-      .then(res => setMyPageDetail(res))
-      .catch(() => setMyPageDetail(MyUserDetailDummy));
-  }, []);
+  const likeProject = useMoreInfo('/api/v1/likes/mylike/project', 'teamSearchResponseList');
+  const likeStudy = useMoreInfo('/api/v1/likes/mylike/study', 'teamSearchResponseList');
+  const likeUser = useMoreInfo('/api/v1/likes/mylike/user', 'userCardResponses');
 
   return (
     <>
@@ -34,22 +26,22 @@ function MyLike() {
             <div className='header_layout'>
               <h2>좋아요 한 프로젝트</h2>
             </div>
-
-            <Link to='/create/team'>프로젝트 만들기</Link>
           </div>
 
           <div className='card_layout'>
-            {!myPageDetail.projects?.length ? (
+            {!likeProject.data.teamSearchResponseList.length ? (
               <div className='list_no_contents'>
                 <p>아직 좋아요 한 프로젝트가 없습니다.</p>
               </div>
             ) : (
               <div>
-                {myPageDetail.projects.map((project) => project && (
+                {likeProject.data.teamSearchResponseList.map(project => project && (
                   <ProjectCard key={project.id} {...project} setLoginDialog={setIsLoginDialogOpen}/>
                 ))}
               </div>
             )}
+            { likeProject.data.hasNextSlice &&
+              <button className='more_button' onClick={likeProject.loadMoreData}>더보기</button> }
           </div>
         </div>
 
@@ -58,29 +50,118 @@ function MyLike() {
             <div className='header_layout'>
               <h2>좋아요 한 스터디</h2>
             </div>
-
-            <Link to='/create/team?teamType=1'>스터디 만들기</Link>
           </div>
 
           <div className='card_layout'>
-            {!myPageDetail.studies?.length ? (
+            {!likeStudy.data.teamSearchResponseList?.length ? (
               <div className='list_no_contents'>
-                <p>좋아요 한 스터디가 없습니다.</p>
+                <p>아직 좋아요 한 스터디가 없습니다.</p>
               </div>
             ) : (
               <div>
-                {myPageDetail.studies.map((project) => project && (
+                {likeStudy.data.teamSearchResponseList.map(project => project && (
                   <ProjectCard key={project.id} {...project} setLoginDialog={setIsLoginDialogOpen}/>
                 ))}
               </div>
             )}
+            { likeStudy.data.hasNextSlice &&
+              <button className='more_button' onClick={likeStudy.loadMoreData}>더보기</button> }
           </div>
+        </div>
+
+        <div className='study'>
+          <div className='header_flex'>
+            <div className='header_layout'>
+              <h2>좋아요 한 유저</h2>
+            </div>
+          </div>
+
+          <div className='card_layout'>
+            {!likeUser.data.userCardResponses?.length ? (
+              <div className='list_no_contents'>
+                <p>아직 좋아요 한 유저가 없습니다.</p>
+              </div>
+            ) : (
+              <div>
+                {likeUser.data.userCardResponses.map(user => user && (
+                  <UserCard key={user.id} {...user} setLoginDialog={setIsLoginDialogOpen}/>
+                ))}
+              </div>
+            )}
+          </div>
+          { likeUser.data.hasNextSlice &&
+            <button className='more_button' onClick={likeUser.loadMoreData}>더보기</button> }
         </div>
       </div>
 
       <Footer/>
     </>
   );
+}
+
+interface IInitData {
+  teamSearchResponseList: any[];
+  userCardResponses: any[];
+  size: number;
+  hasNextSlice: boolean;
+}
+
+const InitData: IInitData = {teamSearchResponseList:[], userCardResponses:[], size: 0, hasNextSlice: true}
+const DefaultPageSize = 6;
+
+function useMoreInfo(apiUrl: string, tagName: 'teamSearchResponseList'|'userCardResponses') {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(InitData);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    fetchNextData().then();
+  }, []);
+
+  function loadMoreData() {
+    fetchNextData().then();
+  }
+
+  async function fetchNextData() {
+    // console.log('loadMoreData', searchParams.page);
+    if (loading || !data.hasNextSlice) return; // 이미 로딩 중이면 중복 호출 방지
+
+    setLoading(true);
+    try {
+      const newData :any = await Api.fetch2Json(apiUrl + `?page=${page}&size=${DefaultPageSize}`);
+
+      const startArrIndex = DefaultPageSize * page;
+      const ArrSize = startArrIndex + newData.size;
+
+      setData(prevData => ({
+        ...prevData,
+        [tagName]:
+          InfScroll.getExpandArray(
+            prevData[tagName],
+            newData[tagName],
+            startArrIndex, ArrSize),
+        size: ArrSize,
+        hasNextSlice: newData.hasNextSlice
+      }));
+
+      setPage(page + 1);
+    }
+    catch (e) {
+      console.error(e, data.hasNextSlice);
+
+      setData(prevData => ({
+        ...prevData,
+        [tagName]: !prevData[tagName].length ? InitData[tagName] : prevData[tagName],
+        size: prevData.size + 1,
+        hasNextSlice: false
+      }));
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  return {data, loadMoreData};
 }
 
 export default MyLike;
