7d26544f70eb13aa99772bcedb9f2bc8882eed1b
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-10-11 21:53:01 +0900

    Fix: 팀원 검색 API 파라미터 Enum으로 수정

--- src/pages/MainPage/MainMenteePage.tsx
+++ src/pages/MainPage/MainMenteePage.tsx
@@ -8,20 +8,22 @@ import useInfScroll from '../../hooks/useInfScroll.ts';
 import Footer from '../../components/Footer.tsx';
 import LoginRecommendDialog from '../../components/dialogLayout/LoginRecommendDialog.tsx';
 import {IUser, IUserCardList} from '../../constant/interfaces.ts';
-import {ProjectRecruitFields} from '../../constant/selectOptions.ts';
+import {getTechListEng, TechListKor} from '../../components/inputs/SelectStackLevel.tsx';
 import {MapLocationName} from '../../components/svgs/maps/MapRouter.tsx';
 import {mentees} from '../../dummies/dummyData.ts';
 import '../../styles/MainProjectPage.scss';
 
-const SortOptions = ['가입순', '온도순', '좋아요순'];
+const SortOptionsKor = ['가입순', '온도순', '좋아요순'];
+const SortOptionsEng = ['', 'reviewScore', 'likes'];
 const MeetingTypeOptions = ['전체', '온라인', '오프라인'];
+const MeetingTypeOptionsEng = ['', 'ONLINE', 'OFFLINE'];
 const LocationOptions = ['전체', ...MapLocationName];
 
 function MainMenteePage() {
   const [isAdvancedSearchOpened, setIsAdvancedSearchOpened] = useState<boolean>(false);
-  const [selectedUserStack, setSelectedUserStack] = useState<string>(ProjectRecruitFields[0]);
+  const [selectedUserStack, setSelectedUserStack] = useState<string>(TechListKor[0]);
   const [searchKeyword, setSearchKeyword] = useState<string>('');
-  const [selectedSort, setSelectedSort] = useState<string>(SortOptions[0]);
+  const [selectedSort, setSelectedSort] = useState<string>(SortOptionsKor[0]);
   const [selectedMeetingType, setSelectedMeetingType] = useState<string>(MeetingTypeOptions[0]);
   const [selectedLocation, setSelectedLocation] = useState<string>(LocationOptions[0]);
 
@@ -36,16 +38,16 @@ function MainMenteePage() {
   function search() {
     let searchObj = {};
 
-    if (selectedUserStack !== ProjectRecruitFields[0])
-      searchObj = {...searchObj, position: selectedUserStack};
+    if (selectedUserStack !== TechListKor[0])
+      searchObj = {...searchObj, position: getTechListEng(selectedUserStack)};
     if (searchKeyword)
       searchObj = {...searchObj, search: searchKeyword};
 
     if (isAdvancedSearchOpened) {
-      if (selectedSort !== SortOptions[0])
-        searchObj = {...searchObj, orderBy: selectedSort};
+      if (selectedSort !== SortOptionsKor[0])
+        searchObj = {...searchObj, orderBy: SortOptionsEng[SortOptionsKor.indexOf(selectedSort)]};
       if (selectedMeetingType !== MeetingTypeOptions[0])
-        searchObj = {...searchObj, meetingType: selectedMeetingType};
+        searchObj = {...searchObj, meetingType: MeetingTypeOptionsEng[MeetingTypeOptions.indexOf(selectedMeetingType)]};
       if (selectedMeetingType === MeetingTypeOptions[2] && selectedLocation !== LocationOptions[0])
         searchObj = {...searchObj, place: selectedLocation};
     }
@@ -80,7 +82,7 @@ function MainMenteePage() {
           <div className='header_flex'>
             <div>
               <div className='search_layout'>
-                <SelectBox options={ProjectRecruitFields}
+                <SelectBox options={TechListKor}
                            value={selectedUserStack}
                            onChange={value => setSelectedUserStack(value)}/>
                 <input type='text'
@@ -100,7 +102,7 @@ function MainMenteePage() {
                 <div className='search_detail_layout'>
                   <div>
                     <h3>정렬</h3>
-                    <SelectBox options={SortOptions}
+                    <SelectBox options={SortOptionsKor}
                                value={selectedSort}
                                onChange={v => setSelectedSort(v)}/>
                   </div>
