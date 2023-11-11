4db9b979e03326a087bc8968a3e5613d705c24d2
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-11 23:06:24 +0900

    Fix: LoginToken 주석 최적화

--- src/pages/LogoutToken.tsx
+++ src/pages/LogoutToken.tsx
@@ -2,12 +2,9 @@ import {Navigate} from 'react-router-dom';
 // import authControl from '../constant/authControl.ts';
 
 function LogoutToken() {
-  // const redirectUrl = authControl.getRedirectUrl();
   document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  // document.cookie = `tokenExpire=${new Date(Date.now())}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-  
-  // if (!!redirectUrl)
-  //   return ( <Navigate to={redirectUrl}/> );
+
+  //   return ( <Navigate to={authControl.getRedirectUrl()}/> );
   return ( <Navigate to='/'/> );
 }
 
