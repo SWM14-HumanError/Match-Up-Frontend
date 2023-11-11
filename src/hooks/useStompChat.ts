4ac20c0e0f80dc047f569f223f35d3d4a288c3cb
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-11 23:08:02 +0900

    Test: 쿠키에서 로그인 토큰 외 다른 쿠키 삭제

--- src/hooks/useStompChat.ts
+++ src/hooks/useStompChat.ts
@@ -45,14 +45,14 @@ function useStompChat(data: IChattingRoomList) {
         console.error(event);
 
         // document.cookie 에서 token, refreshToken 외 모두 삭제
-        // const cookies = document.cookie.split(';');
-        //
-        // cookies.forEach((cookie) => {
-        //   const key = cookie.split('=')[0].trim();
-        //   if (key === 'token' || key === 'refresh_token') return;
-        //   document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-        // });
-        // console.log('document.cookie', document.cookie);
+        const cookies = document.cookie.split(';');
+
+        cookies.forEach((cookie) => {
+          const key = cookie.split('=')[0].trim();
+          if (key === 'token' || key === 'refresh_token') return;
+          document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+        });
+        console.log('document.cookie', document.cookie);
       },
       onDisconnect: (frame: any) => {
         console.log('onDisconnect!!!!!');
