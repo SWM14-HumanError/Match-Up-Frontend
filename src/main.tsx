36fde8cba7a6222299f9c6662c23d65806628940
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-17 22:57:11 +0900

    Dependency: Sentry 사용 일시 중지

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import * as Sentry from '@sentry/react';
-
-Sentry.init({
-  dsn: 'https://6bc23830d5b174c223efcd1261f17571@o4506213122965504.ingest.sentry.io/4506241198718976',
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ['localhost', /^https:\/\/side-match\.com\/api/],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+// import * as Sentry from '@sentry/react';
+//
+// Sentry.init({
+//   dsn: 'https://6bc23830d5b174c223efcd1261f17571@o4506213122965504.ingest.sentry.io/4506241198718976',
+//   integrations: [
+//     new Sentry.BrowserTracing({
+//       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//       tracePropagationTargets: ['localhost', /^https:\/\/side-match\.com\/api/],
+//     }),
+//     new Sentry.Replay(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, // Capture 100% of the transactions
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
