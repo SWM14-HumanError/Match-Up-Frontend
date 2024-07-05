8b87537c002295f67728ca0ffa46b9d243929ccf
hyunwoo0081 <hyunwoo0081@users.noreply.github.com>
2024-07-05 07:33:33 +0000

    deploy: 415ff83aed25f882f552e8f788e9cf6dada943b2

--- src/hooks/useUserInfo.ts
+++
@@ -1,9 +0,0 @@
-function useUserInfo(nickname: string | null, positionLevel: number | null) {
-  return {
-    isAvailableUser: !!nickname,
-    fixedNickname: nickname ?? '존재하지 않는 사용자',
-    fixedPositionLevel: !!nickname ? positionLevel : null,
-  };
-}
-
-export default useUserInfo;
