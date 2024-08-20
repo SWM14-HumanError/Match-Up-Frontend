7db43d3802c1969073f29b65bcaaf7b99c16f0cf
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-08-20 16:12:21 +0900

    Feat: 관리자 도구에서 스택 한국어로 번역된 부분을 json에 추가하는 기능 추가

--- src/pages/AdminPage/CreateStackImagePage.tsx
+++ src/pages/AdminPage/CreateStackImagePage.tsx
@@ -4,10 +4,17 @@ import {IDeviconJson} from "@constant/interfaces.ts";
 import StackList from '@constant/stackList.ts';
 import '@styles/pages/CreateStackImagePage.scss';
 
+interface IKoNamesJson {
+  tagName: string;
+  koNames: string[];
+}
+
 function CreateStackImagePage() {
   const [notExistStacks, setNotExistStacks] = useState<string[]>([]);
   const [stackInfoList, setStackInfoList] = useState<string>('');
   const [stackList, setStackList] = useState<string>('');
+  const [stackKoreanList, setStackKoreanList] = useState<string>('');
+  const [stackKoAddedList, setStackKoAddedList] = useState<string>('');
 
   // stackInfoList를 변환하여 stackList로 변환합니다.
   useEffect(() => {
@@ -35,8 +42,39 @@ function CreateStackImagePage() {
     catch (e) {setStackList('JSON 형식이 아닙니다. devicon.json 파일을 붙여넣어주세요.');}
   }, [stackInfoList]);
 
-  function copyAll() {
-    navigator.clipboard.writeText(stackList).then(() => {
+  // stackList를 변환하여 stackKoreanList로 변환합니다.
+  useEffect(() => {
+    try {
+      const json: IKoNamesJson[] = JSON.parse(stackKoreanList);
+      console.log(stackKoreanList);
+
+      const stacks = [...StackList.map((stack) => ({...stack, koNames: []}))];
+      json.forEach((stack) => {
+        const index = stacks.findIndex((item) => item.tagName === stack.tagName);
+
+        if (index !== -1) {
+          // @ts-ignore
+          stacks[index].koNames = stack.koNames;
+        }
+      });
+
+      setStackKoAddedList(JSON.stringify(stacks, null, 2)
+        .replace(/"tagID"/g, 'tagID')
+        .replace(/"tagName"/g, 'tagName')
+        .replace(/"altnames"/g, 'altnames')
+        .replace(/"color"/g, 'color')
+        .replace(/"svg"/g, 'svg')
+        .replace(/"koNames"/g, 'koNames')
+        .replace(/"/g, `'`));
+    }
+    catch (e) {
+      console.error(e);
+      setStackKoAddedList('JSON 형식이 아닙니다. devicon.json 파일을 붙여넣어주세요.');
+    }
+  }, [stackKoreanList]);
+
+  function copyAll(text: string) {
+    navigator.clipboard.writeText(text).then(() => {
       alert('복사되었습니다.');
     });
   }
@@ -83,11 +121,36 @@ function CreateStackImagePage() {
 
         <div className='header_flex'>
           <h3 style={{display:'inline'}}>변환된 스택 리스트</h3>
-          <button className='stack' onClick={copyAll}>전체 복사</button>
+          <button className='stack' onClick={() => copyAll(stackList)}>전체 복사</button>
         </div>
         <textarea name='' id=''
                   cols={50} rows={10}
                   value={stackList} readOnly/>
+
+        <div className='header_flex'>
+          <h3>스택 제목만 있는 객체</h3>
+          <button className='stack' onClick={() => copyAll(
+            JSON.stringify(StackList.map((stack) => ({'tagName': stack.tagName})))
+          )}>전체 복사</button>
+        </div>
+        <textarea name='' id='' value={JSON.stringify(StackList.map((stack) => ({'tagName': stack.tagName})))} readOnly/>
+
+        <div className='header_flex'>
+          <h3>Korean 저장해봅시다</h3>
+        </div>
+        <textarea name='' id=''
+                  cols={50} rows={10}
+                  value={stackKoreanList}
+                  onChange={e => setStackKoreanList(e.target.value)}/>
+
+        <div className='header_flex'>
+          <h3 style={{display: 'inline'}}>변환된 스택 리스트</h3>
+          <button className='stack' onClick={() => copyAll(stackKoAddedList)}>전체 복사</button>
+        </div>
+
+        <textarea name='' id='' value={stackKoAddedList} readOnly/>
+
+
       </div>
     </>
   );
