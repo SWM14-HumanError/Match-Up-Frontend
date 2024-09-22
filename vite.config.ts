5593d69fd3aa388c278c642f096a84b1ff871449
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-09-22 22:51:04 +0900

    Chore: Proxy 설정 방식 Env로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,77 +1,77 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { defineConfig } from 'vite'
+import {ConfigEnv, defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
+export default ({mode}: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd());
+  const TargetHost = env.VITE_TARGET_HOST ?? 'localhost:8080';
+  const TargetServer = `https://${TargetHost}`;
+  const TargetServerWs = `ws://${TargetHost}`;
 
-const TargetHost = 'localhost:8080';
-// const TargetHost = 'localhost:3000';
-const TargetServer = `http://${TargetHost}`;
-const TargetServerWs = `ws://${TargetHost}`;
-// const TargetServer = 'https://sidematch.co.kr';
-// const TargetServerWs = 'wss://sidematch.co.kr';
+  return defineConfig({
+    define: {
+      'global': {},
+    },
 
-export default defineConfig({
-  define: {
-    'global': {},
-  },
+    plugins: [react(), sentryVitePlugin({
+      org: 'humanerror',
+      project: 'match-up-frontend'
+    })],
 
-  plugins: [react(), sentryVitePlugin({
-    org: 'humanerror',
-    project: 'match-up-frontend'
-  })],
-
-  base: '/',
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'assets'),
-      '@public': path.resolve(__dirname, 'public'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@constant': path.resolve(__dirname, 'src/constant'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@styles': path.resolve(__dirname, 'src/styles')
-    }
-  },
-
-  server: {
-    proxy: {
-      '/api': {
-        target: TargetServer,
-        changeOrigin: true,
-        //rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-        ws: true
-      },
-      // '/login/token/refresh': {
-      //   target: TargetServer,
-      //   changeOrigin: true,
-      //   secure: false,
-      //   ws: true
-      // },
-      '^/(login|logout)$': {
-        target: TargetServer,
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      },
-      '/oauth2': {
-        target: TargetServer,
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      },
-      '/ws-stomp': {
-        target: TargetServerWs,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+    base: '/',
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'assets'),
+        '@public': path.resolve(__dirname, 'public'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@constant': path.resolve(__dirname, 'src/constant'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@styles': path.resolve(__dirname, 'src/styles')
       }
-    }
-  },
+    },
 
-  build: {
-    sourcemap: true
-  }
-})
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: TargetServer,
+          changeOrigin: true,
+          //rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: false,
+          ws: true
+        },
+        // '/login/token/refresh': {
+        //   target: TargetServer,
+        //   changeOrigin: true,
+        //   secure: false,
+        //   ws: true
+        // },
+        '^/(login|logout)$': {
+          target: TargetServer,
+          changeOrigin: true,
+          secure: false,
+          ws: true
+        },
+        '/oauth2': {
+          target: TargetServer,
+          changeOrigin: true,
+          secure: false,
+          ws: true
+        },
+        '/ws-stomp': {
+          target: TargetServerWs,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        }
+      }
+    },
+
+    build: {
+      sourcemap: true
+    }
+  });
+}
